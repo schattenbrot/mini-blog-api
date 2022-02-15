6c8e99a4c9c1cd14c8ad422998ad40780f4187e0
Schattenbrot <aaron.machill@web.de>
2022-02-15 22:48:28 +0100

    refactor password validation, added godoc comments

--- utils/password-validator.go
+++ utils/password-validator.go
@@ -2,6 +2,18 @@ package utils
 
 import "unicode"
 
+const PASSWORD_MIN_LENGTH = 8
+const PASSWORD_MAX_LENGTH = 24
+
+// PasswordIsValid checks for a password validity.
+//
+// Returns false if:
+//   - the password is too short (less than 8 characters)
+//   - the password is too long (more than 24 characters)
+//   - the password contains no upper case character
+//   - the password contains no lower case character
+//   - the password contains no number
+//   - the password contains no special character
 func PasswordIsValid(password string) bool {
 	hasCorrectLength := false
 	hasUpper := false
@@ -9,7 +21,7 @@ func PasswordIsValid(password string) bool {
 	hasNumber := false
 	hasSpecial := false
 
-	if len(password) >= 8 && len(password) <= 24 {
+	if passwordLengthIsValid(password) {
 		hasCorrectLength = true
 	}
 
@@ -27,4 +39,14 @@ func PasswordIsValid(password string) bool {
 	}
 
 	return hasCorrectLength && hasUpper && hasLower && hasNumber && hasSpecial
+}
+
+func passwordLengthIsValid(password string) bool {
+	if len(password) < PASSWORD_MIN_LENGTH {
+		return false
+	}
+	if len(password) > PASSWORD_MAX_LENGTH {
+		return false
+	}
+	return true
 }
