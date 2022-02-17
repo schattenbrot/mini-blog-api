c35f9a1040244dbedacfaf283ee39a1bb260e804
Schattenbrot <aaron.machill@web.de>
2022-02-17 16:40:16 +0100

    posts are not anonymous anymore!

--- models/models.go
+++ models/models.go
@@ -7,6 +7,7 @@ type Post struct {
 	ID        string    `json:"id,omitempty"`
 	Title     string    `json:"title,omitempty" validate:"omitempty,min=3,max=40"`
 	Text      string    `json:"text,omitempty" validate:"omitempty,min=5,max=700"`
+	User      string    `json:"user,omitempty" validate:"omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
@@ -17,6 +18,6 @@ type User struct {
 	Name      string    `json:"name" validate:"omitempty,min=3,max=20"`
 	Email     string    `json:"email" validate:"omitempty,email"`
 	Password  string    `json:"password,omitempty" validate:"omitempty,min=8,max=24"`
-	Roles     []string  `json:"roles"`
+	Roles     []string  `json:"roles" validate:"omitempty,dive,eq=user|admin"`
 	CreatedAt time.Time `json:"created_at"`
 }
